f344e4e4f1747f39634aefc629d31ad090d252de
Jyot189 <jyotkhant2002@gmail.com>
2022-09-04 15:46:06 +0530

    upload

--- upload.js
+++ upload.js
@@ -43,21 +43,19 @@ http.createServer(function (req, res) {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
       var oldpath = files.filetoupload.filepath;
-      var newpath = 'D:/CODE/NodeJs/basic/upload/' + files.filetoupload.originalFilename;
+      var newpath = 'upload/' + files.filetoupload.originalFilename;
       fs.rename(oldpath, newpath, function (err) {
         if (err) throw err;
         res.write('File uploaded and moved!');
-        return res.end();
+        res.end();
       });
  });
   } else {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="POST" enctype="multipart/form-data">');
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
     res.write('<input type="file" name="filetoupload"><br>');
     res.write('<input type="submit">');
     res.write('</form>');
     return res.end();
   }
-}).listen(3000,()=>{
-  console.log("server is running");
-});
+}).listen(3000);
